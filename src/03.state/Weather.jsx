c41ee06863b62ded4117535eb488ef20dd272849
gtto05 <xander0627@outlook.com>
2021-12-04 11:23:28 +0800

    useState

--- src/03.state/Weather.jsx
+++ src/03.state/Weather.jsx
@@ -1,27 +1,42 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Weather extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {isHot: true}
-    this.changeWeather = this.changeWeather.bind(this)
+// export default class Weather extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {isHot: true}
+//     this.changeWeather = this.changeWeather.bind(this)
+//   }
+
+//   changeWeather() {
+//     const { isHot } = this.state
+//     this.setState({
+//       isHot:!isHot
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         今天天气很{this.state.isHot ? '炎热':'凉爽'} <button onClick={this.changeWeather}>changeWeather</button>
+//       </div>
+//     )
+//   }
+// }
+
+import React, { useState } from 'react'
+
+export default function Weather() {
+  const [isHot, setisHot] = useState(true)
+
+  function changeWeather() {
+    setisHot(!isHot)
   }
 
-  changeWeather() {
-    const { isHot } = this.state
-    this.setState({
-      isHot:!isHot
-    })
-  }
-
-  
-
-  render() {
-    return (
-      <div>
-        今天天气很{this.state.isHot ? '炎热':'凉爽'} <button onClick={this.changeWeather}>changeWeather</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      今天天气很{isHot ? '炎热' : '凉爽'} <button onClick={changeWeather}>changeWeather</button>
+    </div>
+  )
 }
+
 
